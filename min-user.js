04423dda9e0a6c9bd29ea3ad5ae721f922061578
spekies <179425982+spekies@users.noreply.github.com>
2024-10-02 22:24:21 -0400

    minimal user.js for personal config

---
+++ min-user.js
@@ -0,0 +1,37 @@
+// startup
+/*
+about:config warning
+*/
+user_pref("browser.aboutConfig.showWarning", false);
+/*
+welcome screen
+*/
+user_pref("browser.aboutwelcome.enabled", false);
+user_pref("browser.startup.homepage_override.mstone", "ignore");
+/*
+tutorial
+*/
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+// Mozilla
+/*
+general
+*/
+user_pref("browser.preferences.moreFromMozilla", false);
+user_pref("identity.fxaccounts.enabled", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.getAddons.showPane", false);
+/*
+addons telem
+*/
+user_pref("extensions.htmlaboutaddons.inline-options.enabled", false);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+/*
+data collecting
+*/
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+/*
+AI features
+*/
+user_pref("browser.preferences.experimental", false);
