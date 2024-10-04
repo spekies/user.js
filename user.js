8c1cbf7c180b2c9794f60a414c2c014d878e9524
spekies <179425982+spekies@users.noreply.github.com>
2024-10-03 20:12:32 -0400

    Update user.js

--- user.js
+++ user.js
@@ -1,38 +1,16 @@
 // startup
-/*
-about:config warning
-*/
-user_pref("browser.aboutConfig.showWarning", false);
-/*
-welcome screen
-*/
-user_pref("browser.aboutwelcome.enabled", false);
-user_pref("browser.startup.homepage_override.mstone", "ignore");
-/*
-tutorial
-*/
-user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
-user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
-
+user_pref("browser.aboutConfig.showWarning", false); // about:config warning
+user_pref("browser.aboutwelcome.enabled", false); // welcome screen
+user_pref("browser.startup.homepage_override.mstone", "ignore"); // welcome screen after update
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false); // tutorial
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false); // tutorial
 // Mozilla
-/*
-disables sync, pocket, and more from Mozilla in about:preferences
-*/
-user_pref("browser.preferences.moreFromMozilla", false);
-user_pref("identity.fxaccounts.enabled", false);
-user_pref("extensions.pocket.enabled", false);
-/*
-addons telem in about:addons
-*/
-user_pref("extensions.getAddons.showPane", false);
-user_pref("extensions.htmlaboutaddons.inline-options.enabled", false);
-user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
-/*
-data collecting in about:preferences
-*/
-user_pref("app.shield.optoutstudies.enabled", false);
-user_pref("datareporting.healthreport.uploadEnabled", false);
-/*
-AI features in about:preferences
-*/
-user_pref("browser.preferences.experimental", false);
+user_pref("browser.preferences.moreFromMozilla", false); // disables more from Mozilla in about:preferences
+user_pref("identity.fxaccounts.enabled", false); // disables sync
+user_pref("extensions.pocket.enabled", false); // disables pocket
+user_pref("extensions.getAddons.showPane", false); // addons telem in about:addons
+user_pref("extensions.htmlaboutaddons.inline-options.enabled", false); // addons telem in about:addons
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false); // addons telem in about:addons
+user_pref("app.shield.optoutstudies.enabled", false); // data collecting in about:preferences
+user_pref("datareporting.healthreport.uploadEnabled", false); // data collecting in about:preferences
+user_pref("browser.preferences.experimental", false); // AI features in about:preferences
